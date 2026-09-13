c21520e3e91aab11e9a163aca90ff3bf72ac07f0
MoDev228 <modev228@gmail.com>
2026-09-13 14:29:43 +0000

    Initialisation du projet

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
